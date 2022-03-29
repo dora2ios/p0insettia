c7fc8623694955557803e410c0edc35e0037c6e2
Dora2 <42214504+dora2-iOS@users.noreply.github.com>
2022-03-30 00:30:38 +0900

    add flag for 7.1/7.1.1

--- untether/make_gen_fw.sh
+++ untether/make_gen_fw.sh
@@ -1,9 +1,15 @@
 #/bin/sh
 
+#define
+
+BUILD="BUILD_11D257"
+#BUILD="BUILD_11D201"
+#BUILD="BUILD_11D167"
+
 rm gen_fw
 
 cd ../Injector
-clang untether.c partialzip/partial.c static/*.a -o gen_fw -I./include -framework Security -framework LDAP -Os -DN42 -DHAVE_IBOOT_EXPLOIT -DBUILD_11D257
+clang untether.c partialzip/partial.c static/*.a -o gen_fw -I./include -framework Security -framework LDAP -Os -DN42 -DHAVE_IBOOT_EXPLOIT -D"$BUILD"
 strip gen_fw
 
 mv gen_fw ../untether
